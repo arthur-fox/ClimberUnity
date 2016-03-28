6580c39248e03b71d0901fc700907b5f87e75c7e
NeoSouldier <FoxBookAir@FoxBookAir.local>
2016-03-28 15:21:47 +0100

    Tweaked InGame Score text size and UV scrolling to keep Platforms inline with Background
    Scores can now fit up to 4 digits (eg. 1000) even though I doubt anyone
    would get that high, its more of a just incase as it wasn’t able to
    display more than 2 digits before (which is clearly wrong as I’ve
    gotten over 100 before).
    
    I tweaked a magic number in the UV Scrolling “Background.cs” script,
    which was meant to represent the size of the texture (brought it down
    from 13.66 to 13.5 as this seems to keep the Platforms more inline with
    the scrolling).

--- Assets/ScrollingBackground.cs
+++ Assets/ScrollingBackground.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ScrollingBackground : MonoBehaviour 
 {
-	const float kHeight = 13.66f; // TODO: Don't just have this as a Magic number based on height of sprites
+	const float kHeight = 13.5f; //13.666f; // TODO: Don't just have this as a Magic number based on height of sprites
 	const string kTextureName = "_MainTex";
 
 	public float m_speed = 1.0f;
